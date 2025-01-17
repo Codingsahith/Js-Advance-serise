109d4033b14db6a93be8011ed6132fb9dea47638
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2025-01-17 06:47:29 +0000

    object literals

---
+++ Advance-js/object.js
@@ -0,0 +1,18 @@
+// objects are collection of properties.
+// property => (key,value)
+const student = {
+    name : "sahith",
+    age : 21,
+    city : "hyd"
+};
+// console.log(student)
+// console.log(student.name)
+// console.log(student.age)
+// console.log(student.city)
+
+let info = [{
+    firstName : "firelord",
+    area : "kp",
+    pincode : 82798
+}];
+// console.log(info)
