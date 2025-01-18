9b64deb169dbd8868c4acc40bc76585017a4a00c
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2025-01-18 06:35:08 +0000

    functions

---
+++ Advance-js/functions.js
@@ -0,0 +1,15 @@
+function myName(){
+    console.log("hello world");
+}
+myName();
+
+function num(sum){
+    sum = 2+2;
+    console.log(sum)
+}
+num();
+
+function adding(a,b){
+    console.log(a+b);
+}
+adding(2,3);
