df774fbd017a821f3d9ee683520c4707b1938eef
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2025-01-20 14:05:30 +0000

    functions

--- Advance-js/functions.js
+++ Advance-js/functions.js
@@ -1,15 +1,22 @@
 function myName(){
     console.log("hello world");
 }
-myName();
+// myName();
 
 function num(sum){
     sum = 2+2;
-    console.log(sum)
+    // console.log(sum)
 }
 num();
 
 function adding(a,b){
     console.log(a+b);
 }
-adding(2,3);
+// adding(2,3);
+
+// return method
+function sum(x,y){
+    return x+y;
+}
+sum(3,2);
+
